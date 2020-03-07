bc328b9a6c1fe21c3dd007e0f49c89a78dbfd22d
gtodur <gtodur@outlook.com>
2020-03-07 09:31:07 +0530

    first commit

---
+++ src/test/java/com/df/springboot/access/jdbc/SpringBootAccessJdbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.df.springboot.access.jdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAccessJdbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
